37383834d3c0a5b4bf20f278f20bfa36d7ac70cc
FiedorPW <mikolajfiedorczuk@gmail.com>
2024-09-05 22:04:23 +0200

    czyszczenie jakiegoś syfu

--- mock_FrontEnd/src/views/Cockpit.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <main class="cockpit-page">
-    <h1>Cockpit</h1>
-    <p>Tu beda linki do cockpitu</p>
-  </main>
-</template>
--- mock_FrontEnd/src/views/Gallery.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <main class="gallery-page">
-        <h1>Galeria</h1>
-        <p>Tu bedzie galeria</p>
-    </main>
-</template>
